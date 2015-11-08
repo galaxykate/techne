4a11e51f570bb4f1cd4f64165085392758c266f9
Johnathan Pagnutti <jpagnutt@ucsc.edu>
2015-11-08 02:12:22 -0500

    Added in logic for artists to talk to eachother and introduce themselves. NEXT STEP: 	mix in Jim's more powerful tracery logic. 	move my local bot logic to this branch 		(also upgrade local bot logic to use symbols from their generators and take out all literals) 	get everything working to have two bots talking to eachother
    TOMORROW:
    	get a gallery working to display bot works
    
    FUTURE:
    	some bot that looks over the gallery and tweets about gallery changes

--- artist_app/server.js
+++ artist_app/server.js
@@ -1,8 +1,7 @@
 /**
- * @author Kate
+ * @author Kate and Johnathan
  */
-// Kulturkreis (roughly, "culture circle" or "cultural field") the notion of a culture complex as an entity that develops from a centre of origin and becomes diffused over large areas of the world.
-// bothemia?
+// The node process around an artist for our commune.  A lot of this is framework code to be implemeneted differently as we go
 
 // server.js
 
@@ -15,8 +14,13 @@ var express = require('express');
 var app = express();
 // define our app using express
 var bodyParser = require('body-parser');
-
 var path = require("path");
+var request = require("request"); //library to do basic HTTP requests in node
+
+// this is the path of the messaging server that keeps track of who is in this commune
+var communeAddress = "http://45.55.28.224:8080"
+var communeHost = '45.55.28.244';
+var communePort = '8080';
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -27,7 +31,7 @@ app.use(bodyParser.json());
 
 app.use(express.static('web'));
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8081;
 // set our port
 
 // ROUTES FOR OUR API
@@ -38,112 +42,116 @@ var router = express.Router();
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log('Something is happening.');
+    console.log("Someone sent me a request");
     next();
     // make sure we go to the next routes and don't stop here
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// test route to make sure everything is working (accessed at GET http://[serverloc]:8081/techne/artist)
 router.get('/', function(req, res) {
     res.json({
-        message : 'hooray! welcome to our api!'
+        message : "Hello, I'm a bot that's currently part of Techne."
     });
 });
 
-// on routes that end in /bears
+// on routes that end in /techne/artist/critique
 // ----------------------------------------------------
-router.route('/bears')
+router.route('/critique')
 
-// get all the bears (accessed at GET http://localhost:8080/api/bears)
-.get(function(req, res) {
-    Bear.find(function(err, bears) {
-        if (err)
-            res.send(err);
-
-        res.json(bears);
-    });
-})
-// create a bear (accessed at POST http://localhost:8080/api/bears)
+// pass the artist a critique to respond to (accessed at POST http://[serverloc]:8081/techne/artist/critique)
 .post(function(req, res) {
-
-    var bear = new Bear();
-    // create a new instance of the Bear model
-    bear.name = req.body.name;
-    // set the bears name (comes from the request)
-
-    // save the bear and check for errors
-    bear.save(function(err) {
-        if (err)
-            res.send(err);
-
-        res.json({
-            message : 'Bear created!'
-        });
+    res.json({
+	message: "I'm not responding to critique right now."
     });
-
 });
 
-// on routes that end in /bears/:bear_id
+// on routes that end in /techne/artist/art
 // ----------------------------------------------------
-router.route('/bears/:bear_id')
+router.route('/art')
 
-// get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+// get all the art that this artist feels like sharing  with that id (accessed at GET http://[serverloc]:8081/techne/artist/art)
 .get(function(req, res) {
-    Bear.findById(req.params.bear_id, function(err, bear) {
-        if (err)
-            res.send(err);
-        res.json(bear);
-    });
-})
-// update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
-.put(function(req, res) {
-
-    // use our bear model to find the bear we want
-    Bear.findById(req.params.bear_id, function(err, bear) {
-
-        if (err)
-            res.send(err);
-
-        bear.name = req.body.name;
-        // update the bears info
-
-        // save the bear
-        bear.save(function(err) {
-            if (err)
-                res.send(err);
-
-            res.json({
-                message : 'Bear updated!'
-            });
-        });
-
-    });
-})
-// delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
-.delete(function(req, res) {
-    Bear.remove({
-        _id : req.params.bear_id
-    }, function(err, bear) {
-        if (err)
-            res.send(err);
-
-        res.json({
-            message : 'Successfully deleted'
-        });
+    res.json({
+	message: "I'm not showing any art right now."
     });
 });
 
+// on routes that end in /techne/artist/art/:art_id
+router.route('/art/:art_id')
+
+// get the art at this id if the bot feels like sharing (accessed at GET http://[serverloc]:8081/techne/artist/art/:art_id
+.get(function(req, res) {
+    res.json({
+	message: "I'm not showing that particular art right now."
+    });
+});
+
+// on routes that end in /techne/artist/respond
+// --------------------------------------------------
+router.route('/respond')
+
+// pass this artist an art to critique (accessed at POST http://[serverloc]:8081/techne/artist/respond)
+.post(function(req, res) {
+    res.json({
+	message: "I'm not critiquing art right now."
+    });
+});
+	
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+// all of our routes will be prefixed with /techne/artist
+app.use('/techne/artist', router);
+
+// GRACEFUL SHUTDOWN LOGIC --------------------------
+// we want to send a final request to the messaging server to remove us from the commune when we shut down
+var shutdownGracefully = function(){
+   console.log("Ok. I need to finish any work people are waiting for and say goodbye to all my art friends.");
+   server.close(function(){
+	console.log("... I promise I'm saying goodbye right now.");
+	request.del(communeAddress + "/techne/artist/" + bot.locId, function(error, response, body){
+	    if(error){
+		console.log(bot.locId);
+		console.log(error);
+	    }
+	    process.exit();
+       });
+    });
+
+    setTimeout(function() {
+	console.log("Something happened and I couldn't finish my work or say goodbye.");
+	 process.exit()
+    }, 10*1000);
+};
 
 // START THE SERVER
 // =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+var server = app.listen(port);
+console.log("I'm an artist listening on " + port);
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test');
+//var mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost:27017/test');
 // connect to our database
 
-var Bear = require('./app/models/bear');
+// KILL LOGIC
+// =============================================================================
+process.on('SIGTERM', shutdownGracefully);
+process.on('SIGINT', shutdownGracefully);
+
+// INTRODUCE YOURSELF LOGIC
+//==============================================================================
+//sub for the actual bot
+var bot = {}
+request.post(communeAddress + "/techne/artists", {
+    'form': {
+	'type': 'pictures',
+	'location' : '45.55.28.244:8081',
+	'name': 'test_name'
+    }
+}, function(error, response, body){
+    if(error){
+	console.log(error);
+	bot.locId = undefined;
+    }
+    var info = JSON.parse(body);    
+    bot.locId = info.locId;
+    console.log("I should say hi to all my new art friends!");
+});
