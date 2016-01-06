98b8f562d8301beea4ca9d96a3d45b4608d0d49b
Johnathan Pagnutti <Johnathan@eduroam-169-233-194-190.ucsc.edu>
2016-01-06 12:33:18 -0800

    Current distributed branch

--- gallery_app/js/app.js
+++ gallery_app/js/app.js
@@ -35,50 +35,64 @@ function createCard(data) {
 }
 
 
+function getAllArt(artistList){
+    //we can now chain into the next request, which is to ask each artist for their art
+    var holder = $("#artists");
+    for(var i = 0; i < artistList.length; i++){
+        var artistLoc = artistList[i].location;
+        //ask for all the artist's art
+        $.get(artistLoc + "techne/artist/art", function(data, status, xhr){
+            if(status == "success"){
+                console.log("Got an artist's art!");
+                console.log(data);
+                var div = $("<div/>", {
+                    class : "artist card",
+                }).appendTo(holder);
+
+                var card = createCard({
+                    title : data[0].artist.name //We should get back an art array, although a smart person would check this first.  Also, this should be undefined.
+                });
+                card.div.appendTo(holder);
+                card.artHolder = $("<div/>", {
+                    class : "art-holder"
+                }).appendTo(card.contents);
+
+                card.artHolder.html("");
+                //add the returned art to the newly created holder
+                $.each(data, function(index, art){
+                    var artCard = createCard({
+                        title : art.title
+                    });
+                    artCard.div.addClass("minicard");
+                    artCard.div.appendTo(artHolder);
+                    artCard.contents.append(art.content);
+                });
+            }else{
+                console.log("Failure to get an artist's art");
+                console.log(status);
+                console.log(data);
+            }
+        });
+    }
+}
+
 $(document).ready(function() {
     //start by getting all the artists currently in the commune.
-    $.get(communeAddress + "/techne/artists", function(data, status, xhr){
-        if(status == "success"){
-            //we can now chain into the next request, which is to ask each artist for their art
-            var holder = $("#artists");
-            var artistList = data;
-            for(var i = 0; i < artistList.length; i++){
-                var artistLoc = artistList[i].location;
-                //ask for all the artist's art
-                $.get(artistLoc + "techne/artist/art", function(data, status, xhr){
-                    if(status == "success"){
-                        var div = $("<div/>", {
-                            class : "artist card",
-
-                        }).appendTo(holder);
-
-                        var card = createCard({
-                            title : data[0].artist.name; //We should get back an art array, although a smart person would check this first.  Also, this should be undefined.
-                        });
-                        card.div.appendTo(holder);
-                        card.artHolder = $("<div/>", {
-                            class : "art-holder"
-                        }).appendTo(card.contents);
-
-                        card.artHolder.html("");
-                        //add the returned art to the newly created holder
-                        $.each(data, function(index, art){
-                            var artCard = createCard({
-                                title : art.title
-                            });
-                            artCard.div.addClass("minicard");
-                            artCard.div.appendTo(artHolder);
-                            artCard.contents.append(art.content);
-                        }
-                    }else{
-                        console.log("Failure to get an artist's art");
-                        console.log(status);
-                        console.log(data);
-                    }
-                }
-            }
-        }else{
-            console.log("Failure to get list of artists in the commune.");
+    console.log("Attempting to get a list of artists from");
+    console.log(communeAddress + "/techne/artists");
+    $.ajax({
+        type: 'GET',
+        url: communeAddress + "/techne/artists",
+        cache: false,
+        dataType: 'text',
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Got an error..."); 
+            console.log(textStatus);
+            console.log(errorThrown);
+            console.log(jqXHR);
+        },
+        success: function(data, status, xhr){
+            console.log("Got at list of artists!");
             console.log(status);
             console.log(data);
         }
