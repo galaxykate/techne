0d3f955ae99f173a50526e5412198f278d901b20
Johnathan Pagnutti <Johnathan@Acrux.local>
2016-07-26 00:28:56 -0700

    Minor fixes.  Commented out unused window

--- web/js/bot.js
+++ web/js/bot.js
@@ -50,20 +50,20 @@ var Bot = Class.extend({
 				bot.happiness += qual;
 			});
 		});
-bot.happiness *= .07;
-console.log(bot.happiness);
-$(".bot-view" + this.id + " .bot-happiness").html(bot.happiness.toFixed(2) + evalToEmoji(bot.happiness));
+		bot.happiness *= 0.07;
+		console.log(bot.happiness);
+		$(".bot-view" + this.id + " .bot-happiness").html(bot.happiness.toFixed(2) + evalToEmoji(bot.happiness));
 	},
 
 	toSVGPortrait: function() {
-		var headR = portraitSize * .36;
-		var r0 = headR * (.7 - .4 * this.dna[9]);
+		var headR = portraitSize * 0.36;
+		var r0 = headR * (0.7 - 0.4 * this.dna[9]);
 		var r1 = headR * 1.2;
 		var r2 = headR * 1.1;
-		var tilt = -.3 + .6 * utilities.noise(20 * this.dna[2] + 20);
-		var subR = r0 * .9;
-		var outerTilt0 = -.4 + .8 * utilities.noise(20 * this.dna[0]);
-		var outerTilt1 = -.4 + .8 * utilities.noise(20 * this.dna[1] + 10);
+		var tilt = -0.3 + 0.6 * utilities.noise(20 * this.dna[2] + 20);
+		var subR = r0 * 0.9;
+		var outerTilt0 = -0.4 + 0.8 * utilities.noise(20 * this.dna[0]);
+		var outerTilt1 = -0.4 + 0.8 * utilities.noise(20 * this.dna[1] + 10);
 		var theta0 = -Math.PI / 2;
 
 		var theta1 = theta0 + 1.2;
@@ -74,18 +74,18 @@ $(".bot-view" + this.id + " .bot-happiness").html(bot.happiness.toFixed(2) + eva
 		var p3 = Vector.polar(r1, tilt + theta3);
 		var cp0a = Vector.polarOffset(p0, subR, tilt + theta0 - Math.PI / 2);
 		var cp0b = Vector.polarOffset(p0, subR, tilt + theta0 + Math.PI / 2);
-		var cp1a = Vector.polarOffset(p1, subR, tilt + Math.PI + theta1 + outerTilt0)
-		var cp1b = Vector.polarOffset(p1, subR, tilt + theta1 + outerTilt0)
-		var cp2a = Vector.polarOffset(p2, subR * 1.5, tilt + theta0 + Math.PI / 2)
-		var cp2b = Vector.polarOffset(p2, subR * 1.5, tilt + theta0 - Math.PI / 2)
+		var cp1a = Vector.polarOffset(p1, subR, tilt + Math.PI + theta1 + outerTilt0);
+		var cp1b = Vector.polarOffset(p1, subR, tilt + theta1 + outerTilt0);
+		var cp2a = Vector.polarOffset(p2, subR * 1.5, tilt + theta0 + Math.PI / 2);
+		var cp2b = Vector.polarOffset(p2, subR * 1.5, tilt + theta0 - Math.PI / 2);
 		var cp3a = Vector.polarOffset(p3, subR, tilt + theta3 + outerTilt1);
 		var cp3b = Vector.polarOffset(p3, subR, tilt + Math.PI + theta3 + outerTilt1);
 
 
-		var b0 = Vector.polar(r2 * .8, tilt + theta0);
-		var b1 = Vector.polarOffset(p2, r2 * (.3 + .3 * this.dna[5]), tilt + theta0)
-		var cb0 = Vector.polarOffset(b1, subR * .8, tilt + theta0 - Math.PI / 2)
-		var cb1 = Vector.polarOffset(b1, subR * .8, tilt + theta0 + Math.PI / 2)
+		var b0 = Vector.polar(r2 * 0.8, tilt + theta0);
+		var b1 = Vector.polarOffset(p2, r2 * (0.3 + 0.3 * this.dna[5]), tilt + theta0);
+		var cb0 = Vector.polarOffset(b1, subR * 0.8, tilt + theta0 - Math.PI / 2);
+		var cb1 = Vector.polarOffset(b1, subR * 0.8, tilt + theta0 + Math.PI / 2);
 
 		var h = (this.dna[3] * 360);
 		var s = Math.round(60 + 30 * this.dna[7]);
@@ -176,7 +176,7 @@ $(".bot-view" + this.id + " .bot-happiness").html(bot.happiness.toFixed(2) + eva
 
 		var happiness = $("<div/>", {
 			class: "data bot-happiness",
-}).appendTo(happinessHolder);
+		}).appendTo(happinessHolder);
 
 		var criticHolder = $("<div/>", {
 			class: "bot-criticholder"
